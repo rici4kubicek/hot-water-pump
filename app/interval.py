9149db57b22f684ad768bbe5b49b4e14b55944b0
Richard Kubíček <rici.kubicek@gmail.com>
2021-07-29 21:43:32 +0200

    Add interval.py lib

---
+++ app/interval.py
@@ -0,0 +1,65 @@
+"""
+Interval/periodic measurements
+"""
+
+import time
+
+__all__ = ["Interval"]
+
+
+class Interval(object):
+    __slots__ = ('_start_time', '_reload')
+
+    def __init__(self, value=0, expired=False):
+        self._start_time = int(time.monotonic())
+        self._reload = value
+        if expired:
+            self._start_time -= self._reload
+
+    def __str__(self):
+        return 'Interval(period={},remain={},expired={})'.format(self._reload, self.reminding, self.is_expired)
+
+    @property
+    def reminding(self):
+        """
+        Reminding time to expiration in seconds
+        :return:
+        """
+        if self._reload is None:
+            return 0
+        return self._reload - (int(time.monotonic() - self._start_time))
+
+    @property
+    def is_expired(self):
+        """
+        Check if interval is expired
+        :return:
+        """
+        if self._reload is None:
+            return None
+        return int(time.monotonic()) >= self._start_time + self._reload
+
+    def restart(self):
+        """
+        Restart timer from now
+        """
+        self._start_time = int(time.monotonic())
+
+    def set(self, value):
+        """
+        Set reload value and start interval
+        :param value:
+        :return:
+        """
+        self._reload = value
+        self.restart()
+
+    def restarted(self):
+        """
+        Check if interval is expired and if so, auto reload
+        :return:
+        """
+        if self.is_expired:
+            self.restart()
+            return True
+        return False
