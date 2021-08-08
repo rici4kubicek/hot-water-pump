f512a275682e00bd462dbf6f79e45d2601c3ad53
Richard Kubíček <rici.kubicek@gmail.com>
2021-08-08 20:50:34 +0200

    Fix Interval lib

--- app/interval.py
+++ app/interval.py
@@ -2,7 +2,7 @@
 Interval/periodic measurements
 """
 
-import time
+from .monotonic import monotonic
 
 __all__ = ["Interval"]
 
@@ -11,7 +11,7 @@ class Interval(object):
     __slots__ = ('_start_time', '_reload')
 
     def __init__(self, value=0, expired=False):
-        self._start_time = int(time.monotonic())
+        self._start_time = int(monotonic())
         self._reload = value
         if expired:
             self._start_time -= self._reload
@@ -27,7 +27,7 @@ class Interval(object):
         """
         if self._reload is None:
             return 0
-        return self._reload - (int(time.monotonic() - self._start_time))
+        return self._reload - (int(monotonic() - self._start_time))
 
     @property
     def is_expired(self):
@@ -37,13 +37,13 @@ class Interval(object):
         """
         if self._reload is None:
             return None
-        return int(time.monotonic()) >= self._start_time + self._reload
+        return int(monotonic()) >= self._start_time + self._reload
 
     def restart(self):
         """
         Restart timer from now
         """
-        self._start_time = int(time.monotonic())
+        self._start_time = int(monotonic())
 
     def set(self, value):
         """
